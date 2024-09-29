fcd2753b6fc9de56e9fc62c9cf25655ad74d6405
Frank Wunderlich <frank-w@public-files.de>
2024-09-29 19:29:52 +0200

    build_hostapd: drop make config as it leads to an error

--- build_hostapd.sh
+++ build_hostapd.sh
@@ -29,8 +29,7 @@ if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/. 2>/dev/null)" ]; th
 	cp defconfig .config
 	sed -i 's/#CONFIG_SAE/CONFIG_SAE/' .config
 	sed -i 's/#CONFIG_ACS/CONFIG_ACS/' .config
-	make config
-	make
+	make -j4
 	file hostapd hostapd_cli
 	tar -czf hostapd.tar.gz hostapd hostapd_cli
 
